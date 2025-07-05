eb381aa02d7357fc6a96f9d95944d04442f02395
Pavle Maksimović <163150525+MaksPavle@users.noreply.github.com>
2025-07-05 17:19:03 +0200

    Ispravio cms.go

--- cms/cms.go
+++ cms/cms.go
@@ -1,55 +1,147 @@
-package main
+package cms
 
 import (
-	"encoding/json"
+	"encoding/binary"
+
 	"fmt"
-	"hash/fnv"
-	"io/ioutil"
+	"math"
+	"os"
 )
 
-type CountMinSketch struct {
-	HashFunctions int     `json:"hash_functions"`
-	Table         [][]int `json:"table"`
-	Width         int     `json:"width"`
+// CMS predstavlja Count-Min Sketch strukturu
+type CMS struct {
+	Matrix    [][]uint32     // k redova, m kolona
+	Depth     uint           // broj hash funkcija (k)
+	Width     uint           // broj kolona (m)
+	HashFuncs []HashWithSeed // hash funkcija sa 32-bajt seed-ovima
 }
 
-func NewCountMinSketch(hashFunctions, width int) *CountMinSketch {
-	table := make([][]int, hashFunctions)
-	for i := range table {
-		table[i] = make([]int, width)
+// NewCMS kreira novi CMS na osnovu zeljene greske i verovatnoce
+func NewCMS(epsilon, delta float64) *CMS {
+	width := CalculateM(epsilon)
+	depth := CalculateK(delta)
+
+	matrix := make([][]uint32, depth)
+	for i := range matrix {
+		matrix[i] = make([]uint32, width)
 	}
-	return &CountMinSketch{HashFunctions: hashFunctions, Table: table, Width: width}
-}
 
-func hash(value string, seed int) int {
-	h := fnv.New32a()
-	h.Write([]byte(fmt.Sprintf("%d-%s", seed, value)))
-	return int(h.Sum32())
-}
+	hashFuncs := CreateHashFunctions(depth)
 
-func (cms *CountMinSketch) Update(value string) {
-	for i := 0; i < cms.HashFunctions; i++ {
-		index := hash(value, i) % cms.Width
-		cms.Table[i][index]++
+	return &CMS{
+		Matrix:    matrix,
+		Depth:     depth,
+		Width:     width,
+		HashFuncs: hashFuncs,
 	}
 }
 
-func (cms *CountMinSketch) Serialize(filename string) error {
-	data, err := json.Marshal(cms)
+// Add povecava brojanje za dati kljuc
+func (cms *CMS) Add(key string) {
+	data := []byte(key)
+
+	for i := uint(0); i < cms.Depth; i++ {
+		index := cms.HashFuncs[i].Hash(data) % uint64(cms.Width)
+		cms.Matrix[i][index]++
+	}
+}
+
+// Estimate vraca procenjeno pojavljivanje kljuca
+func (cms *CMS) Estimate(key string) uint32 {
+	data := []byte(key)
+	min := uint32(math.MaxUint32)
+	for i := uint(0); i < cms.Depth; i++ {
+		index := cms.HashFuncs[i].Hash(data) % uint64(cms.Width)
+		val := cms.Matrix[i][index]
+		if val < min {
+			min = val
+		}
+	}
+	return min
+}
+
+// SaveToFile binarno snima CMS u fajl
+func (cms *CMS) SaveToFile(path string) error {
+	file, err := os.Create(path)
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(filename, data, 0644)
+	defer file.Close()
+
+	// Snimi dimenzije
+	if err := binary.Write(file, binary.LittleEndian, uint32(cms.Depth)); err != nil {
+		return err
+	}
+	if err := binary.Write(file, binary.LittleEndian, uint32(cms.Width)); err != nil {
+		return err
+	}
+
+	// Snimi sve hash seedove (32 bajta svaki)
+	for _, hf := range cms.HashFuncs {
+		if len(hf.Seed) != 32 {
+			return fmt.Errorf("seed nije 32 bajta")
+		}
+		if _, err := file.Write(hf.Seed); err != nil {
+			return err
+		}
+	}
+
+	// Snimi celu matricu
+	for i := uint(0); i < cms.Depth; i++ {
+		for j := uint(0); j < cms.Width; j++ {
+			if err := binary.Write(file, binary.LittleEndian, cms.Matrix[i][j]); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
 
-func Deserialize(filename string) (*CountMinSketch, error) {
-	data, err := ioutil.ReadFile(filename)
+// LoadFromFile učitava CMS iz fajla
+func LoadFromFile(path string) (*CMS, error) {
+	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
-	var cms CountMinSketch
-	if err := json.Unmarshal(data, &cms); err != nil {
+	defer file.Close()
+
+	var depth32, width32 uint32
+	if err := binary.Read(file, binary.LittleEndian, &depth32); err != nil {
 		return nil, err
 	}
-	return &cms, nil
+	if err := binary.Read(file, binary.LittleEndian, &width32); err != nil {
+		return nil, err
+	}
+
+	depth := uint(depth32)
+	width := uint(width32)
+
+	// Učitaj hash funkcije (32 bajta po seedu)
+	hashFuncs := make([]HashWithSeed, depth)
+	for i := uint(0); i < depth; i++ {
+		seed := make([]byte, 32)
+		if _, err := file.Read(seed); err != nil {
+			return nil, err
+		}
+		hashFuncs[i] = HashWithSeed{Seed: seed}
+	}
+
+	// Učitaj matricu
+	matrix := make([][]uint32, depth)
+	for i := range matrix {
+		matrix[i] = make([]uint32, width)
+		for j := uint(0); j < width; j++ {
+			if err := binary.Read(file, binary.LittleEndian, &matrix[i][j]); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return &CMS{
+		Matrix:    matrix,
+		Depth:     depth,
+		Width:     width,
+		HashFuncs: hashFuncs,
+	}, nil
 }
